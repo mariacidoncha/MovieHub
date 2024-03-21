76c6c2d63b406f68d478ae93e4efb80c5ba3047b
Maria Cidoncha <mariacidoncha@gmail.com>
2024-03-21 10:24:01 +0100

    config server && config folders

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './server';
+import config from './config/config';
+
+const PORT = config.app.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
